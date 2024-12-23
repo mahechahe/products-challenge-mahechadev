7a41ca813f6ec06395af36d5635285b5c5c598ed
estiven-mahecha-tooles <estivenmahecha@tooles.co>
2024-12-23 15:30:09 -0500

    [ADD] Agrego de carrito de compras

---
+++ src/app/components/DrawerShoppingCart/DrawerShoppingCart.jsx
@@ -0,0 +1,136 @@
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from '@mui/material';
+
+const products = [
+  {
+    id: 1,
+    name: 'Throwback Hip Bag',
+    href: '#',
+    color: 'Salmon',
+    price: '$90.00',
+    quantity: 1,
+    imageSrc:
+      'https://tailwindui.com/plus/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+    imageAlt:
+      'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+  },
+  {
+    id: 2,
+    name: 'Medium Stuff Satchel',
+    href: '#',
+    color: 'Blue',
+    price: '$32.00',
+    quantity: 1,
+    imageSrc:
+      'https://tailwindui.com/plus/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+    imageAlt:
+      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+  },
+];
+
+function DrawerShoppingCart({ handleCloseModal }) {
+  return (
+    <div
+      className="h-full w-[400px] bg-white gap-4 grid"
+      style={{
+        gridTemplateRows: '1.6fr 0.4fr',
+        height: '100dvh',
+      }}
+    >
+      <div className="w-full px-5 py-5">
+        <div className="w-full flex items-center justify-between">
+          <h2
+            className="text-lg text-gray-900"
+            style={{
+              fontWeight: 500,
+            }}
+          >
+            Carrito de compras
+          </h2>
+
+          <IconButton onClick={handleCloseModal}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+
+        <div className="mt-8">
+          <div className="flow-root">
+            <ul role="list" className="-my-6 divide-y divide-gray-200">
+              {products.map((product) => (
+                <li key={product.id} className="flex py-6">
+                  <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
+                    <img
+                      alt={product.imageAlt}
+                      src={product.imageSrc}
+                      className="size-full object-cover"
+                    />
+                  </div>
+
+                  <div className="ml-4 flex flex-1 flex-col">
+                    <div>
+                      <div className="flex justify-between text-base font-medium text-gray-900">
+                        <h3>
+                          <a href={product.href}>{product.name}</a>
+                        </h3>
+                        <p className="ml-4">{product.price}</p>
+                      </div>
+                      <p className="mt-1 text-sm text-gray-500">
+                        {product.color}
+                      </p>
+                    </div>
+                    <div className="flex flex-1 items-end justify-between text-sm">
+                      <p className="text-gray-500">Qty {product.quantity}</p>
+
+                      <div className="flex">
+                        <button
+                          type="button"
+                          className="font-medium text-indigo-600 hover:text-indigo-500"
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="w-full ">
+        <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+          <div className="flex justify-between text-base font-medium text-gray-900">
+            <p>Subtotal</p>
+            <p>$262.00</p>
+          </div>
+          <p className="mt-0.5 text-sm text-gray-500">
+            Envío e impuestos calculados al realizar el pago.
+          </p>
+          <div className="mt-6">
+            <a
+              href="#"
+              className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+            >
+              Pagar con tarjeta de crédito
+            </a>
+          </div>
+          <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+            <p>
+              o{' '}
+              <button
+                type="button"
+                onClick={handleCloseModal}
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Seguir comprando
+                <span aria-hidden="true"> &rarr;</span>
+              </button>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DrawerShoppingCart;
